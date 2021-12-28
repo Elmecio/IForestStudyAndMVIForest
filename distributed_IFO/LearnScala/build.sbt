badbcbdb3b680e92badc140222338329ec9366f9
Maurras <maurras.togbe@isep.fr>
2021-12-28 17:11:55 +0100

    Suppression

--- distributed_IFO/LearnScala/build.sbt
+++
@@ -1,5 +0,0 @@
-name := "LearnScala"
-
-version := "0.1"
-
-scalaVersion := "2.13.5"
--- distributed_IFO/python_spark_tutorial/pairRdd/aggregation/reducebykey/housePrice/AvgCount.py
+++
@@ -1,7 +0,0 @@
-class AvgCount():
-
-    def __init__(self, count: int, total: float):
-        self.count = count
-        self.total = total
-
-
